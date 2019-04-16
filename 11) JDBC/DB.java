3a180b46514ef6dd43bba564f3b07a3323c27865
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-04-16 08:11:17 -0700

    Database completely connectedgit add .

--- 11) JDBC/DB.java
+++ 11) JDBC/DB.java
@@ -1,4 +1,5 @@
 import java.sql.*; //1) import package after adding jdbc jar
+//import oracle.jdbc.*;
 class DB
 {
 public static void main(String args[])
@@ -8,11 +9,11 @@ try //2) always work in try block for exceptions
 		Class.forName("oracle.jdbc.driver.OracleDriver"); //3) initialize driver
 		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","SYSTEM","12345678"); //4) Create connection object
 		Statement st = con.createStatement(); //5) Create statement object
-		String sql = "select * from nish";
+		String sql = "select * from courses";
 		ResultSet rs = st.executeQuery(sql); //6) Create result set object to invoke the select statement result
 		while(rs.next())
 		{
-		System.out.println(rs.getInt(1));
+		System.out.println(rs.getString(1) + " "  + rs.getString(2));
 		}
 		con.close(); //7) Close the connection object
 		}
